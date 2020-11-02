ccf6a4e4a74c637655fa1e61a1276984410a3e27
ChenXuOffWorld <chen.xu@offworld.ai>
2020-11-02 11:23:18 -0800

    First commit for code base

---
+++ build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/offworld/Desktop/workspace/catkin_ws/src/my_robot
+# Build directory: /home/offworld/Desktop/workspace/catkin_ws/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
