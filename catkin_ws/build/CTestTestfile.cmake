6dc3ef6ccebae5be67efa7d3c1e4767176891d60
xc-fighting <cxu264@usc.edu>
2021-06-13 12:32:07 -0700

    Update:update the ros to noetic and put packages into catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/xuchen/Desktop/robotic_programs/catkin_ws/src
+# Build directory: /home/xuchen/Desktop/robotic_programs/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("simple_arm")
+subdirs("ball_chaser")
---
+++ catkin_ws/build/ball_chaser/CMakeFiles/drive_bot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/xuchen/Desktop/robotic_programs/catkin_ws/devel/lib/ball_chaser/drive_bot"
+  "/home/xuchen/Desktop/robotic_programs/catkin_ws/devel/lib/ball_chaser/drive_bot.pdb"
+  "CMakeFiles/drive_bot.dir/src/drive_bot.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drive_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
